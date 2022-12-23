9bab73a7d333f9b0c559f86c9461a92e5cb25559
Danilo Serafim <danilob.serafim@gmail.com>
2022-12-23 10:06:53 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const button = document.getElementById("btnGerar")
+
+button.addEventListener("click", ()=>{
+    let titulo = document.getElementById("titulo").value
+    let descricao = document.getElementById("descricao").value
+    let instalacao = document.getElementById("instalacao").value
+    let uso = document.getElementById("uso").value
+    let contribuir = document.getElementById("contribuir").value
+    let licenca = document.getElementById("licenca").value
+    let creditos = document.getElementById("creditos").value
+    let mais = document.getElementById("mais").value
+
+    let blob = new File([`
+        # Título
+        ${titulo}
+
+        ## Descrição
+        ${descricao}
+
+        ## instalação
+        ${instalacao}
+
+        ## Modo de uso
+        ${uso}
+
+        ## ajude você tambêm
+        ${contribuir}
+
+        ## Licença
+        ${licenca}
+
+        ## Créditos
+        ${creditos}
+
+        ## Um pouco mais
+        ${mais}
+
+        
+
+        
+    `], 'README.md',{type:"text/plain"})
+    let url = window.URL.createObjectURL(blob)
+
+
+    let a = document.createElement("a");
+        a.href = url;
+        a.download = blob.name;
+        a.click();
+        window.URL.revokeObjectURL(url);
+
+
+
+    
+
+})
