00fc6e1a10637616890e7fb8b63cff452705635f
Danilo Serafim <danilob.serafim@gmail.com>
2022-12-29 13:02:36 -0300

    Melhorando usabilidade

--- script.js
+++ script.js
@@ -1,42 +1,74 @@
 const button = document.getElementById("btnGerar")
+const inputTitulo = document.getElementById("titulo")
+const inputDescricao = document.getElementById("descricao")
+const alert = document.getElementById("alert")
+
+function change() {
+    inputDescricao.style.borderColor = "gray"
+    inputDescricao.style.borderWidth = '1px'
+
+    inputTitulo.style.borderColor = "gray"
+    inputTitulo.style.borderWidth = '1px'
+
+    alert.style.opacity = 0
+
+}
 
 function separaLinhas(dado) {
-    return dado.replaceAll('\n', '</br>\n')
+    return dado.replaceAll('\n', '</br>\n    ')
 }
 
 
 button.addEventListener("click", ()=>{
-    let banner = document.getElementById("banner").value
     let titulo = document.getElementById("titulo").value
     let descricao = document.getElementById("descricao").value
+    let banner = document.getElementById("banner").value
     let instalacao = document.getElementById("instalacao").value
+    let tecnologias = document.getElementById("tecnologias").value
     let uso = document.getElementById("uso").value
     let contribuir = document.getElementById("contribuir").value
     let licenca = document.getElementById("licenca").value
     let creditos = document.getElementById("creditos").value
     let mais = document.getElementById("mais").value
 
-    let file = new File([`<h1>${titulo}</h1>
-    <img src='${banner}' alt="Banner" width='100%'/>
+    if (!titulo || !descricao) {
+        alert.style.opacity = 1
+        if (!descricao) {
+            inputDescricao.style.borderWidth = '2px'
+            inputDescricao.style.borderColor = 'red'
+        }
+        if (!titulo) {
+            inputTitulo.style.borderWidth = '2px'
+            inputTitulo.style.borderColor = 'red'
+        }
+        return
+    }
+    
+
+    let file = new File([`    <h1>${titulo}</h1>
     <p>${separaLinhas(descricao)}</p></br>
+    ${banner && `<img src='${banner}' alt="Banner" width='100%'/>`}
 
-    <h2>instalação</h2>
-    <p>${separaLinhas(instalacao)}</p>
+    ${instalacao && `<h2>instalação</h2>
+    <p>${separaLinhas(instalacao)}</p>`}
 
-    <h2>Modo de uso</h2>
-    <p>${separaLinhas(uso)}</p>
+    ${uso && `<h2>Modo de uso</h2>
+    <p>${separaLinhas(uso)}</p>`}
 
-    <h2>ajude você tambêm</h2>
-    <p>${separaLinhas(contribuir)}</p>
+    ${tecnologias && `<h2>Técnologias</h2>
+    <p>${separaLinhas(tecnologias)}</p>`}
 
-    <h2>Licença</h2>
-    <p>${licenca}</p>
+    ${contribuir && `<h2>ajude você tambêm</h2>
+    <p>${separaLinhas(contribuir)}</p>`}
 
-    <h2>Créditos</h2>
-    <p>${separaLinhas(creditos)}</p>
+    ${licenca && `<h2>Licença</h2>
+    <p>${licenca}</p>`}
 
-    <h2>Um pouco mais</h2>
-    <p>${separaLinhas(mais)}</p>`], 'README.md',{type: "text/plain;charset=utf-8"})
+    ${creditos && `<h2>Créditos</h2>
+    <p>${separaLinhas(creditos)}</p>`}
+
+    ${mais && `<h2>Um pouco mais</h2>
+    <p>${separaLinhas(mais)}</p>`}`], 'README.md',{type: "text/plain;charset=utf-8"})
     let url = window.URL.createObjectURL(file)
 
 
